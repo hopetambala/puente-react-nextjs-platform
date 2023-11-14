14b6c21ec13782ea297f15ec9e3a9ce9cca38a8c
Hope Tambala <hopetambala@outlook.com>
2023-11-13 23:56:16 -0500

    chore: add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+/** @type { import('@storybook/react').Preview } */
+import "app/impacto-design-system/_css/root.css";
+
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
