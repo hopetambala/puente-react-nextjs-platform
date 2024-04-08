ad1ae820724aa004a67f1f33b9822ecd427e695f
Hope Tambala <hopetambala@outlook.com>
2024-04-07 22:43:37 -0400

    chore: catch typescript nonsense

--- app/epics/FormManager/TableV2/_helpers.tsx
+++ app/epics/FormManager/TableV2/_helpers.tsx
@@ -6,8 +6,8 @@ export type FormRow = {
   id: string
   name: string
   status: React.ReactNode
-  created: string
-  updated: string
+  createdAt: string
+  updatedAt: string
   // actions: React.ReactNode
 }
 
@@ -16,33 +16,33 @@ interface Cell { getValue: () => any }
 const columnHelper = createColumnHelper<FormRow>()
 
 export const columns = (organization: string) => [
-  columnHelper.accessor('name', {
+  columnHelper.accessor("name", {
     cell: (info: Cell) => info.getValue(),
     header: () => <span>Name</span>,
   }),
-  columnHelper.accessor('status', {
+  columnHelper.accessor("status", {
     cell: () => <Tag color="green" text="Published" />,
     header: () => <span>Status</span>,
     enableSorting: false,
   }),
   columnHelper.accessor(
-    (row: { createdAt: FormRow['created'] }) => row.createdAt,
+    (row: { createdAt: FormRow["createdAt"] }) => row.createdAt,
     {
-      id: 'createdAt',
+      id: "createdAt",
       cell: (info: Cell) => info.getValue(),
       header: () => <span>Created</span>,
     }
   ),
   columnHelper.accessor(
-    (row: { updatedAt: FormRow['updated'] }) => row.updatedAt,
+    (row: { updatedAt: FormRow["updatedAt"] }) => row.updatedAt,
     {
-      id: 'updatedAt',
+      id: "updatedAt",
       cell: (info: Cell) => info.getValue(),
       header: () => <span>Updated</span>,
     }
   ),
   columnHelper.display({
-    id: 'download',
+    id: "download",
     cell: (row: any) => (
       <CSVButton form={row.row.original} surveyingOrganization={organization} />
     ),
